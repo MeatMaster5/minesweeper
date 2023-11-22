82e9c48457b93e08924dc2703f9669c98cb0f22c
Sam <eg6735kv@go.minnstate.edu>
2023-11-21 20:20:21 -0600

    initial comments

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import grid
+#each tile is seither a blank or contains a mine
 
+#each tile is also either flagged, hidden, or revealed
 
-print(grid.generate())
+#if player reveals a mine tile they lose
+#if player reveals every blank tile they win
+
