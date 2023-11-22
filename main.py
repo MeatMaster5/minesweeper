d08160791c33c934b540798d43eaab617930c996
Sam <eg6735kv@go.minnstate.edu>
2023-11-21 20:29:16 -0600

    throatlantis

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-#each tile is seither a blank or contains a mine
+#to the game, each tile is seither a blank or contains a mine
 
-#each tile is also either flagged, hidden, or revealed
+#to the user, each tile is also either flagged, hidden, or revealed
+#revealed tiles also have a number on them which means adjacent mines
 
 #if player reveals a mine tile they lose
 #if player reveals every blank tile they win
+#on revealing a blank, reveal all other adjacent blanks recursively
 
+#0=blank
+#1=mine
+grid =[
+    [0,0,0,0,1,0],
+    [0,1,1,0,0,0],
+    [0,0,0,0,0,0],
+    [0,1,0,0,1,0],
+    [0,0,0,1,0,0],
+    [0,0,0,0,0,0],
+]
